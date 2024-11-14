c5cd4e2c284fa195411f704e41ca50a70271c6b7
Charles7c <charles7c@126.com>
2024-11-14 21:24:17 +0800

    chore: 优化任务调度服务端配置

--- continew-extension/continew-extension-schedule-server/src/main/java/top/continew/admin/extension/scheduling/ScheduleServerApplication.java
+++ continew-extension/continew-extension-schedule-server/src/main/java/top/continew/admin/extension/scheduling/ScheduleServerApplication.java
@@ -16,8 +16,16 @@
 
 package top.continew.admin.extension.scheduling;
 
+import cn.hutool.core.net.NetUtil;
+import cn.hutool.core.util.URLUtil;
+import cn.hutool.extra.spring.SpringUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 
 /**
  * 任务调度服务端启动程序
@@ -25,10 +33,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author KAI
  * @since 2024/6/25 22:24
  */
+@Slf4j
 @SpringBootApplication
-public class ScheduleServerApplication {
+@RequiredArgsConstructor
+public class ScheduleServerApplication extends com.aizuda.snailjob.server.SnailJobServerApplication implements ApplicationRunner {
+
+    private final ServerProperties serverProperties;
 
     public static void main(String[] args) {
-        SpringApplication.run(com.aizuda.snailjob.server.SnailJobServerApplication.class, args);
+        SpringApplication.run(ScheduleServerApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) {
+        String hostAddress = NetUtil.getLocalhostStr();
+        Integer port = serverProperties.getPort();
+        String contextPath = serverProperties.getServlet().getContextPath();
+        String baseUrl = URLUtil.normalize("%s:%s%s".formatted(hostAddress, port, contextPath));
+        log.info("----------------------------------------------");
+        log.info("{} service started successfully.", SpringUtil.getApplicationName());
+        log.info("API 地址：{}", baseUrl);
+        log.info("----------------------------------------------");
     }
 }
